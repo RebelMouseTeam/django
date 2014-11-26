f1ad5c5bce7f6c0a102682ce4c367bd0e5f7c640
Andrey Gubarev <mylokin@me.com>
2014-11-26 12:54:07 +0300

    update tests #21952

--- tests/dispatch/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Unit-tests for the dispatch project
-"""
-
-from __future__ import absolute_import
-
-from .test_dispatcher import DispatcherTests, ReceiverTestCase
--- tests/regressiontests/dispatch/tests/__init__.py
+++ tests/regressiontests/dispatch/tests/__init__.py
@@ -5,4 +5,3 @@ Unit-tests for the dispatch project
 from __future__ import absolute_import
 
 from .test_dispatcher import DispatcherTests, ReceiverTestCase
-from .test_saferef import SaferefTests
